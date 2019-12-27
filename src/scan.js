0c37da526295712ce0cfa24d023baf425d575588
Josh Greenberg <joshgreenberg91@gmail.com>
2019-12-27 14:15:51 -0800

    Add many more offers, scrape changes in footnotes but only broadcast summary, dynamic pages

--- src/scan.js
+++ src/scan.js
@@ -4,61 +4,110 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const puppeteer = require('puppeteer')
 const Diff = require('diff')
 
-const virtualDOM = async (url) => {
-  const result = await axios.get(url)
-  return cheerio.load(result.data)
+const virtualDOM = async (offer, page) => {
+  if (offer.clickSelector) {
+    await page.goto(offer.url)
+    await page.click(offer.clickSelector)
+    return cheerio.load(await page.content())
+  } else {
+    const result = await axios.get(offer.url)
+    return cheerio.load(result.data)
+  }
 }
 
-const compare = (oldText, newText) => {
-  return Diff.diffSentences(oldText, newText)
+const extractSummary = ($, selectors) => {
+  let output = ''
+  selectors.forEach(selector => {
+    const $selector = $(selector)
+    $selector.find('sup').remove()
+    $selector.find('.subScript').remove()
+    const extractedText = $selector.text().trim()
+    if (extractedText) {
+      output += extractedText.replace(/(\s\s+)/g, "\n")
+    }
+  })
+  return output
 }
 
-const formatMarkdown = (name, diff) => {
-  let output = `*${name}*\n`
+const formatMarkdown = (offer, diff) => {
+  let output = `*${offer.name}*\n`
   diff.forEach(part => {
     const text = part.value.replace(/([*_])/g, '')
     if (part.removed) {
       output += `*-* ${text}\n`
     } else if (part.added) {
       output += `*+* _${text}_\n`
-    } else {
+    } else if (part.count > 0) {
       output += `\n`
     }
   })
+  output += `[See offer details](${offer.url})`
   return output
+}
+
+const sendTelegram = async (offer, diff) => {
+  await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_API_TOKEN}/sendMessage`, {
+    chat_id: process.env.TELEGRAM_CHAT_ID,
+    text: formatMarkdown(offer, diff),
+    parse_mode: 'Markdown'
+  })
+}
+
+const dispatch = async (offer, diff) => {
+  await sendTelegram(offer, diff)
 }
 
 const main = async () => {
   await db.connect()
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
   const file = fs.readFileSync(`${__dirname}/data/offers.yaml`, 'utf8')
   const offers = yaml.safeLoad(file)
 
-  const select = `SELECT body FROM offers WHERE offers.name = $1 ORDER BY timestamp DESC LIMIT 1`
-  const insert = `INSERT INTO offers(name, timestamp, body) VALUES($1, $2, $3)`
+  const select = `SELECT summary, footnotes FROM offers WHERE offers.name = $1 ORDER BY timestamp DESC LIMIT 1`
+  const insert = `INSERT INTO offers(name, timestamp, summary, footnotes) VALUES($1, $2, $3, $4)`
   await offers.asyncForEach(async (offer) => {
-    let oldText = ''
+    let oldFootnotes = ''
+    let oldSummary = ''
+    let newFootnotes = ''
+    let newSummary = ''
     await db.query(select, [offer.name], (err, result) => {
       if (result.rows.length > 0) {
-        oldText = result.rows[0].body
+        oldSummary = result.rows[0].summary
+        oldFootnotes = result.rows[0].footnotes
       }
     })
-    const $ = await virtualDOM(offer.url)
-    const newText = $(offer.selector).text().trim()
-    if (oldText != newText) {
-      await db.query(insert, [offer.name, Date.now(), newText], () => {})
-      const diff = compare(oldText, newText)
-      await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_API_TOKEN}/sendMessage`, {
-        chat_id: process.env.TELEGRAM_CHAT_ID,
-        text: formatMarkdown(offer.name, diff),
-        parse_mode: 'Markdown'
+
+    const $ = await virtualDOM(offer, page)
+    if (offer.footnotesSelector) {
+      const footnotes = []
+      $(offer.footnotesSelector).each((i, el) => {
+        footnotes.push($(el).text().trim())
       })
+      newFootnotes = footnotes.join("\n")
+    }
+
+    if (newFootnotes === '') {
+      const selectors = offer.selectors || [offer.selector].filter(x => x)
+      newSummary = extractSummary($, selectors)
+    } else if (oldFootnotes != newFootnotes) {
+      const selectors = offer.selectors || [offer.selector].filter(x => x)
+      newSummary = extractSummary($, selectors)
+    }
+
+    if (oldFootnotes != newFootnotes || (newFootnotes === '' && oldSummary != newSummary)) {
+      await db.query(insert, [offer.name, Date.now(), newSummary, newFootnotes])
+      const diffPreview = Diff.diffSentences(oldSummary, newSummary)
+      await dispatch(offer, diffPreview)
     }
   })
 
-  db.end()
+  await browser.close()
+  await db.end()
 }
 
 main()
--- src/setup.js
+++ src/setup.js
@@ -1,9 +1,13 @@
 const db = require('./db')
 
 const main = async () => {
-  await db.connect()
-  await db.query("CREATE TABLE offers (id serial PRIMARY KEY, name TEXT, timestamp TEXT, body TEXT)")
-  await db.end()
+  try {
+    await db.connect()
+    await db.query("CREATE TABLE offers (id serial PRIMARY KEY, name TEXT, timestamp TEXT, summary TEXT, footnotes TEXT)")
+    await db.end()
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 main()
