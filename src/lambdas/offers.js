a916d20ca032bd7efd89993744109b74bafe1b2e
Josh Greenberg <joshgreenberg91@gmail.com>
2020-06-26 10:44:45 -0700

    Fixed?

--- src/lambdas/offers.js
+++ src/lambdas/offers.js
@@ -96,10 +96,6 @@ const main = async (argv, { page, db }) => {
   const oldOffers = await db.models.Offer.find()
 
   await offers.asyncForEach(async (offer) => {
-    if (offer.name != 'AMEX Platinum') {
-      return
-    }
-
     const oldOffer = oldOffers
       .filter((o) => o.name === offer.name)
       .sort((a, b) => b.timestamp - a.timestamp)[0]
@@ -138,9 +134,6 @@ const main = async (argv, { page, db }) => {
       // error
       console.log(`[!!!] ${offer.name}: ${err.message}`)
     }
-
-    // TROUBLESHOOTING
-    console.log(footnotes)
 
     if (oldOffer && oldOffer.footnotes.join('\n') === footnotes.join('\n')) {
       // unchanged, skip
