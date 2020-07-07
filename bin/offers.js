8ed25c455c85bf1e9594e9eb952c0cae30c8faa3
Josh Greenberg <joshgreenberg91@gmail.com>
2020-07-06 18:39:32 -0700

    Troubleshooting offers

--- bin/offers.js
+++ bin/offers.js
@@ -5,32 +5,9 @@ const argv = yargs.option('dispatch', { type: 'boolean' })
 const main = async () => {
   const db = require('../src/db')
   const browser = await puppeteer.launch({
-    args: [
-      '--ignore-certificate-errors',
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-accelerated-2d-canvas',
-      '--disable-gpu',
-      '--disable-dv-shm-usage',
-      '--single-process',
-    ],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   })
   const page = await browser.newPage()
-  await page.setUserAgent(
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
-  )
-  await page.setViewport({
-    width: 1200,
-    height: 900,
-  })
-  await page.setRequestInterception(true)
-  page.on('request', (request) => {
-    if (['image', 'stylesheet', 'font'].includes(request.resourceType())) {
-      request.abort()
-    } else {
-      request.continue()
-    }
-  })
 
   const injected = { page, db }
 
